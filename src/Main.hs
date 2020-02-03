90a9ba6faf469101f142b0ae5568ccf6f6b98554
Anton Kalinin <anton.kalinin@bsc-ideas.com>
2020-02-03 21:05:48 +0300

    Join VM.Image to VM

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,17 @@ module Main (main) where
 import           Data.Maybe (fromJust)
 import qualified VM
 import qualified VM.Instruction as I
-import qualified VM.Image
 
 main :: IO ()
 main = do
     print runResult
 
-vm :: VM.VM
-vm = VM.withCode [ I.loadConst 10
-                 , I.loadConst 20
-                 , I.loadConst 50
-                 --, I.end
-                 ]
+code :: [I.Instruction]
+code = [ I.loadConst 10
+       , I.loadConst 20
+       , I.loadConst 50
+       --, I.end
+       ]
 
-runResult :: (VM.Image.RunResult, VM.Image.Image)
-runResult = VM.Image.run 100000000 $ fromJust $ VM.Image.withVM vm
+runResult :: (VM.RunResult, VM.VM)
+runResult = VM.run 100000000 $ fromJust $ VM.withCode code
--- src/VM.hs
+++ src/VM.hs
@@ -1,10 +1,85 @@
-module VM where
+module VM (
+    VM, RunResult,
+    withCode, run
+    ) where
+import           Control.Monad.Trans.State.Strict
+import           Data.Array.Unboxed
 import qualified VM.Instruction as I
 
 type Word = Int
+type Code = Array Int I.Instruction
+type Stack = UArray Int VM.Word
 
-data VM = VM [I.Instruction]
-        deriving (Show)
+data VM = VM { code  :: !Code
+             , stack :: !Stack
+             , sp    :: !Int
+             , pc    :: !Int
+             } deriving (Show)
 
-withCode :: [I.Instruction] -> VM
-withCode = VM
+data StepResult = StepOk | StepEndInstruction
+                  deriving (Show)
+
+data RunResult = RunEnded | RunMaxInstructionsReached
+                 deriving (Show)
+
+stackSize :: Int
+stackSize = 16
+
+withCode :: [I.Instruction] -> Maybe VM
+withCode code
+    | null code = Nothing
+    | otherwise = Just VM { code = listArray (0, length code - 1) code
+                          , stack = listArray (0, stackSize-1) (repeat 0)
+                          , sp = 0
+                          , pc = 0
+                          }
+
+run :: Int -> VM -> (RunResult, VM)
+run maxSteps vm = runState (run' maxSteps) vm
+
+run' :: Int -> State VM RunResult
+run' maxSteps
+    | maxSteps <= 0 = (return RunMaxInstructionsReached)
+    | otherwise     = step >>= \result ->
+          case result of
+              StepOk             -> run' (maxSteps - 1)
+              StepEndInstruction -> (return RunEnded)
+
+step :: State VM StepResult
+step = do
+    instr <- currentInstruction
+    let opcode = I.opcode instr
+        arg = I.arg instr
+        spDelta = I.spDelta instr
+    case opcode of
+        I.LoadConst -> do
+            putToStack 0 arg
+            incSP spDelta
+            incPC 1
+            return StepOk
+        I.End -> do
+            incSP spDelta
+            return StepEndInstruction
+
+currentInstruction :: State VM I.Instruction
+currentInstruction = do
+    VM { code = code, pc = pc } <- get
+    return $ code ! pc
+
+putToStack :: Int -> VM.Word -> State VM ()
+putToStack relIdx value = modify' $ \vm ->
+    vm { stack = (stack vm) // [(stackAbsIndex relIdx vm, value)] }
+
+stackAbsIndex :: Int -> VM -> Int
+stackAbsIndex relIdx vm = (sp vm + relIdx) `mod` stackSize
+
+incSP :: Int -> State VM ()
+incSP increment = modify' $ \vm ->
+    vm { sp = stackAbsIndex increment vm }
+
+incPC :: Int -> State VM ()
+incPC increment = modify' $ \vm ->
+    vm { pc = (pc vm + increment) `mod` codeLength vm }
+
+codeLength :: VM -> Int
+codeLength vm = (+1) . snd . bounds $ code vm
