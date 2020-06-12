41119b612140a67e0d43ac189fe97b3487173c23
Anton Kalinin <anton.kalinin@bsc-ideas.com>
2020-06-12 10:08:04 +0300

    Speed up VM more than twice

--- src/VM.hs
+++ src/VM.hs
@@ -1,14 +1,18 @@
+{-# LANGUAGE RankNTypes #-}
+
 module VM (
     VM, RunResult,
     withCode, run
     ) where
 import           Control.Monad.ST
-import           Data.STRef
 import           Data.Vector.Unboxed.Mutable (MVector)
 import           Data.Vector (Vector)
 import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Generic.Mutable as MV
 import qualified VM.Instruction as I
+import Control.Monad.State
+import Control.Monad.Reader
+import Control.Applicative
 
 type Word = Int
 
@@ -19,12 +23,17 @@ data VM = VM {
     pc    :: !Int
     } deriving (Show)
 
-data MutVM s = MutVM {
-    mutCode    :: {-# UNPACK #-} !(Vector I.Instruction),
-    mutCodeLen :: {-# UNPACK #-} !Int,
-    mutStack   :: {-# UNPACK #-} !(MVector s VM.Word),
-    mutSP      :: {-# UNPACK #-} !(STRef s Int),
-    mutPC      :: {-# UNPACK #-} !(STRef s Int)
+type Run s a = (ReaderT (ROData s) (StateT MutData (ST s)) a)
+
+data ROData s = ROData {
+    roCode    :: {-# UNPACK #-} !(Vector I.Instruction),
+    roCodeLen :: {-# UNPACK #-} !Int,
+    roStack   :: {-# UNPACK #-} !(MVector s VM.Word)
+    }
+
+data MutData = MutData {
+    mutSP     :: {-# UNPACK #-} !Int,
+    mutPC     :: {-# UNPACK #-} !Int
     }
 
 data StepResult = StepOk | StepEndInstruction
@@ -46,49 +55,49 @@ withCode code
                           }
 
 run :: Int -> VM -> (RunResult, VM)
-run maxSteps vm = runST $ do
-    mutVM <- thawVM vm
-    result <- runMutVM maxSteps mutVM
-    newVM <- freezeVM mutVM
-    return (result, newVM)
+run maxSteps vm = withVM vm $ do
+    result <- runMutVM maxSteps
+    vm' <- freezeVM
+    return (result, vm')
 
-thawVM :: VM -> ST s (MutVM s)
-thawVM vm = do
-    mutStack <- V.thaw . V.fromList $ stack vm
-    spRef    <- newSTRef $ sp vm
-    pcRef    <- newSTRef $ pc vm
-    return MutVM {
-        mutCode    = V.fromList $ code vm,
-        mutCodeLen = length $ code vm,
-        mutStack   = mutStack,
-        mutSP      = spRef,
-        mutPC      = pcRef
-        }
+withVM :: VM -> (forall s. Run s a) -> a
+withVM vm action = runST $ do
+    stack <- V.thaw . V.fromList $ stack vm
+    let roData = ROData
+          { roCode = V.fromList $ code vm
+          , roCodeLen = length $ code vm
+          , roStack = stack
+          }
+        mutData = MutData
+          { mutSP = sp vm
+          , mutPC = pc vm
+          }
+    evalStateT (runReaderT action roData) mutData
 
-freezeVM :: MutVM s -> ST s VM
-freezeVM mutVM = do
-    stack <- V.freeze $ mutStack mutVM
-    sp    <- readSTRef $ mutSP mutVM
-    pc    <- readSTRef $ mutPC mutVM
-    return VM {
-        code = V.toList $ mutCode mutVM,
-        stack = V.toList stack,
-        sp = sp,
-        pc = pc
-        }
+freezeVM :: Run s VM
+freezeVM = do
+  roData <- ask
+  mutData <- get
+  stack <- V.freeze $ roStack roData
+  return VM
+      { code = V.toList $ roCode roData
+      , stack = V.toList stack
+      , sp = mutSP mutData
+      , pc = mutPC mutData
+      }
 
-runMutVM :: Int -> MutVM s -> ST s RunResult
-runMutVM maxSteps mutVM
+runMutVM :: Int -> Run s RunResult
+runMutVM maxSteps
     | maxSteps <= 0 = return RunMaxInstructionsReached
     | otherwise = do
-          result <- step mutVM
+          result <- step
           case result of
-              StepOk             -> runMutVM (maxSteps - 1) mutVM
+              StepOk             -> runMutVM (maxSteps - 1)
               StepEndInstruction -> return RunEnded
 
-step :: MutVM s -> ST s StepResult
-step vm = do
-    instr <- currentInstruction vm
+step :: Run s StepResult
+step = do
+    instr <- currentInstruction
 
     let opcode = I.opcode instr
         arg = I.arg instr
@@ -96,36 +105,38 @@ step vm = do
 
     case opcode of
         I.LoadConst -> do
-            putToStack 0 arg vm
-            incSP spDelta vm
-            incPC 1 vm
+            putToStack 0 arg
+            incSP spDelta
+            incPC 1
             return StepOk
         I.End -> do
-            incSP spDelta vm
+            incSP spDelta
             return StepEndInstruction
 
-currentInstruction :: MutVM s -> ST s I.Instruction
-currentInstruction vm = do
-    pc <- readSTRef $ mutPC vm
-    return $ mutCode vm `V.unsafeIndex` pc
+currentInstruction :: Run s I.Instruction
+currentInstruction = liftA2 V.unsafeIndex (asks roCode) (gets mutPC)
 
-putToStack :: Int -> VM.Word -> MutVM s -> ST s ()
-putToStack relIdx value vm = do
-    sp <- readSTRef $ mutSP vm
-    MV.unsafeWrite (mutStack vm) (stackAbsIndex relIdx sp) value
+putToStack :: Int -> VM.Word -> Run s ()
+putToStack relIdx value = do
+    sp <- gets mutSP
+    stack <- asks roStack
+    MV.unsafeWrite stack (stackAbsIndex relIdx sp) value
 
 stackAbsIndex :: Int -> Int -> Int
 stackAbsIndex relIdx sp = (sp + relIdx) `mod` stackSize
 
-incSP :: Int -> MutVM s -> ST s ()
-incSP increment vm = modifySTRef' (mutSP vm) (stackAbsIndex increment)
+incSP :: Int -> Run s ()
+incSP increment = modify' $ \s -> s { mutSP = stackAbsIndex increment (mutSP s) }
 
-incPC :: Int -> MutVM s -> ST s ()
-incPC increment vm = modifySTRef' (mutPC vm) transform
-    where transform pc
-              | increment >= 0 && increment <= codeLen =
-                if increment + pc >= codeLen
-                then increment + pc - codeLen
-                else increment + pc
-              | otherwise = (increment + pc) `mod` codeLen
-          codeLen = mutCodeLen vm
+incPC :: Int -> Run s ()
+incPC increment = do
+    codeLen <- asks roCodeLen
+    modify' $ \s -> s { mutPC = pureIncPC increment codeLen $ mutPC s }
+
+pureIncPC :: Int -> Int -> Int -> Int
+pureIncPC increment codeLen pc
+    | increment >= 0 && increment <= codeLen =
+        if increment + pc >= codeLen
+        then increment + pc - codeLen
+        else increment + pc
+    | otherwise = (increment + pc) `mod` codeLen
